b5f5a464f95b8f9218acdcbf0d719556bc851939
NatasaZvekic <zvekicnatasa123@gmail.com>
2022-06-12 21:15:07 +0200

    add auth server

---
+++ DIS/spring-cloud/authorization-server/src/main/java/sample/OAuth2AuthorizationServerApplication.java
@@ -0,0 +1,18 @@
+package sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Joe Grandja
+ * @since 0.0.1
+ */
+@SpringBootApplication
+public class OAuth2AuthorizationServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OAuth2AuthorizationServerApplication.class, args);
+  }
+
+}
+//CHECKSTYLE:ON
